85d1c03429a119f557c624cd4e7666d23b8af4f6
Opti-kjh <kjh@deali.net>
2025-07-18 00:52:36 +0900

    chore(custom): cz config update
    ISSUES CLOSED: fere-1173

--- .cz-config.js
+++ .cz-config.js
@@ -11,14 +11,16 @@ module.exports = {
   ],
   messages: {
     type: '변경 사항의 타입을 선택하세요:',
-    scope: '변경 범위(선택):',
+    footer: '이슈 번호(선택):',
     subject: '변경 사항을 간결하게 작성하세요:',
     body: '상세한 설명(선택):',
+    scope: '변경 범위(선택):',
     breaking: 'BREAKING CHANGES(선택):',
-    footer: '이슈 번호(선택):',
     confirmCommit: '위 내용으로 커밋할까요?'
   },
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
-  subjectLimit: 100
+  subjectLimit: 100,
+  // 대소문자에 대한 구분없이 입력한 값을 그대로 사용
+  subjectCase: 'none',
 }; 
